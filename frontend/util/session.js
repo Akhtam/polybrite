4b09a70d36486f3b46cf76536dde7f6fd4b77045
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-06 22:44:33 -0800

    delete session util

--- frontend/util/session.js
+++ frontend/util/session.js
@@ -3,5 +3,15 @@ const axios = require('axios');
 export const postUser = user => {
 	return axios.post('/api/users', {
 		user
-	}).then(response => console.log(response))
+	});
+};
+
+export const postSession = user => {
+	return axios.post('/api/session', {
+		user
+	});
+};
+
+export const deleteSession = () => {
+	return axios.delete('/api/session');
 };
