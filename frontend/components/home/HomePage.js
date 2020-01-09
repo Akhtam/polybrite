7e544a7c846c30e43fe4de701078496b2d932b69
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-09 10:49:35 -0800

    create home page header

--- frontend/components/home/HomePage.js
+++ frontend/components/home/HomePage.js
@@ -3,16 +3,28 @@ import React, { Component } from 'react';
 export class HomePage extends Component {
 	render() {
 		return (
-			<div className='main-page'>
-				<header className='home-header'>
-					<main className='name-description'>
-						<h1></h1>
-						<p></p>
-					</main>
-					<aside className='header-img'>
-
-					</aside>
-				</header>
+			<div className='top-header'>
+				<div className='main-page'>
+					<header className='home-header'>
+						<main className='name-description'>
+							<h1>Polybrite</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.
+							</p>
+						</main>
+						<aside>
+							<img
+								className='header-img'
+								src='https://drwfxyu78e9uq.cloudfront.net/usercontent/nutrify/media/images/cedf0c4-colina-organismo.jpg'
+							/>
+						</aside>
+					</header>
+				</div>
 			</div>
 		);
 	}
