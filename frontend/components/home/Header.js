79cb3664943645ab0c1ffbe3c4bc00396485bd3c
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-10 03:39:05 -0800

    d

--- frontend/components/home/Header.js
+++ frontend/components/home/Header.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Header = () => {
 	return (
 		<div className='top-header'>
-			<div className='main-page'>
 				<header className='home-header'>
 					<main className='name-description'>
 						<div className='poly-name-p'>
-							<h1 className='mission-header'>Master new Skills</h1>
+							<h1 className='mission-header'>
+								Master new Skills
+							</h1>
 							<p className='mission-description'>
 								Lorem ipsum dolor sit amet, consectetur
 								adipiscing elit, sed do eiusmod tempor
@@ -18,11 +19,14 @@ const Header = () => {
 						</div>
 					</main>
 					<aside className='header-img'>
-						<img src='https://drwfxyu78e9uq.cloudfront.net/usercontent/nutrify/media/images/cedf0c4-colina-organismo.jpg' />
+						<img
+							src='https://d3b8hk1o42ev08.cloudfront.net/wp-content/uploads/2018/08/52871508-emisferi-cerebrali-a-destra-%C3%A8-emozioni-intuizioni-creativit%C3%A0-sinistra-%C3%A8-la-logica-la-ragione-verbale-1-1-752x440.jpg'
+							className='main-img'
+						/>
 					</aside>
 				</header>
 			</div>
-		</div>
+
 	);
 };
 
