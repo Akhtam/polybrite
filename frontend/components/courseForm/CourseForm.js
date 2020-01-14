e70ab35858d064f69a8f44af69af78c37a274620
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-13 22:43:23 -0800

    ew

--- frontend/components/courseForm/CourseForm.js
+++ frontend/components/courseForm/CourseForm.js
@@ -51,7 +51,7 @@ export default class CourseForm extends Component {
 		formData.append('course[aboutCreator]', this.state.aboutCreator);
 		formData.append('course[categoryId]', this.state.categoryId);
 		formData.append('course[topicId]', this.state.topicId);
-		formData.append('course[courseId]', this.state.courseId);
+
 		this.props
 			.action(formData)
 			.then(res => this.props.history.push(`${res.course.id}`));
