98ce61c91dd16227c6033b72f106968f1fc031bb
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-16 18:09:01 -0800

    style showpage

--- frontend/components/showCourse/CourseShow.js
+++ frontend/components/showCourse/CourseShow.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
 export default class ShowCourse extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			location: {}
+		};
+	}
 	componentDidMount() {
-		this.props.fetchCourse(this.props.match.params.courseId);
+		this.props.fetchCourse(this.props.match.params.courseId).then(res => {
+			this.setState(
+				{
+					location: JSON.parse(res.course.location)
+				},
+				() => console.log(this.state)
+			);
+		});
 	}
 
 	handleEdit(e) {
@@ -13,7 +26,6 @@ export default class ShowCourse extends Component {
 	render() {
 		const {
 			title,
-			location,
 			description,
 			startDate,
 			endDate,
@@ -28,13 +40,14 @@ export default class ShowCourse extends Component {
 			.split(' ')
 			.slice(0, 3);
 		const loctime = new Date(startDate).toLocaleTimeString().split(' ');
-		const time = loctime[0].slice(0, 4) + loctime[1];
+		const time = loctime[0].slice(0, 4) + ' ' + loctime[1];
 		return (
 			<div>
 				<header className='show-header'>
 					<img src={photoUrl} className='header-show-img' />
 				</header>
 				<div className='show-content'>
+					{/* TOOOOOPPPP */}
 					<div className='show-top-content'>
 						<img src={photoUrl} className='main-show-img' />
 						<div className='show-main-info'>
@@ -46,7 +59,61 @@ export default class ShowCourse extends Component {
 							</div>
 							<div className='main-info-title'>
 								<h1>{title}</h1>
+								<span>{this.state.location.venue}</span>
 							</div>
+							<div className='main-info-price'>Free</div>
+						</div>
+					</div>
+					{/* ENROLLLMENT */}
+					<div className='show-button'>
+						<div className='show-button-container'>
+							<button>Enroll</button>
+						</div>
+					</div>
+					{/* DESCription */}
+					<div className='main-description'>
+						<div className='mm-description'>
+							<div className='description'>
+								<div className='desc-content'>
+									<h1>About Course</h1>
+									<span>{description}</span>
+								</div>
+							</div>
+							<div className='date-loc-content'>
+								<h3>Date And Time</h3>
+								<div className='day-time'>
+									{dayMonth.join(' ')} - {time}
+								</div>
+								<h3>Location</h3>
+								<div className='location'>
+									{this.state.location.city}
+								</div>
+								<div className='location'>
+									{this.state.location.venue}
+								</div>
+							</div>
+						</div>
+						<hr className='show-hr' />
+					</div>
+
+					<div className='show-more-info'>
+						<div className='ar-header'>
+							<h1>Requirements</h1>
+						</div>
+						<div className='ar-content'>{requirements}</div>
+						<div className='ar-header'>
+							<h1>About Instructor</h1>
+						</div>
+						<div className='ar-content'>{aboutCreator}</div>
+						<hr className='show-hr' />
+					</div>
+
+					<div className='show-button edit-delete'>
+						<div className='show-button-container delete'>
+							<button>Delete</button>
+						</div>
+						<div className='show-button-container edit'>
+							<button>Edit</button>
 						</div>
 					</div>
 				</div>
