79a1bab7993978c95dec029ec6f70f63b4efca65
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-16 23:04:28 -0800

    add scroll top after redirect

--- frontend/components/root.js
+++ frontend/components/root.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from './ScrollToTop';
 
 export default ({ store }) => (
 	<Provider store={store}>
 		<HashRouter>
+			<ScrollToTop />
 			<App />
 		</HashRouter>
 	</Provider>
