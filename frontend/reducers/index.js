f23fdb36ce777359f6f2e804f9ea32840f4a2c96
Akhtam Ismatov <aismatov1991@gmail.com>
2020-02-18 15:44:17 -0800

    remove modal

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session';
 import errorsReducer from './errors/errorsReducer';
 import coursesReducer from './entities/entities';
-import ui from './ui/uiReducer';
 
 export default combineReducers({
 	entities: coursesReducer,
 	session: sessionReducer,
-	errors: errorsReducer,
-	ui
+	errors: errorsReducer
 });
