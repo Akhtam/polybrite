b347f9be0bca611630e4b34a84881c463ab11921
Akhtam Ismatov <aismatov1991@gmail.com>
2020-03-02 21:26:00 -0800

    add profile route

--- frontend/components/navbar/Dropdown.js
+++ frontend/components/navbar/Dropdown.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
-export const Dropdown = ({ handleLogout, open, user }) => {
+export const Dropdown = ({ handleLogout, redirectToProfile, open, user }) => {
 	let showOrNot = open ? 'block' : 'none';
 	return (
 		<div className='dropdown-container' style={{ display: showOrNot }}>
 			<div className='dropdown-content'>
-				<div className='dropdown-elem user-firstname'>{user.firstName}</div>
+				<div className='dropdown-elem user-firstname'>
+					{user.firstName}
+				</div>
 				<hr />
 
-				<div className='dropdown-elem'>Profile</div>
+				<div className='dropdown-elem' onClick={redirectToProfile}>
+					Profile
+				</div>
 				<div
 					className='dropdown-elem logout-btn'
 					onClick={handleLogout}
