1668dfeae57d528ccf39f748d4b5b8a48d864d99
Akhtam Ismatov <aismatov1991@gmail.com>
2020-03-02 21:34:17 -0800

    change styling

--- frontend/components/profile/Profile.js
+++ frontend/components/profile/Profile.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Profile extends Component {
     render() {
         return (
-            <div>
+            <div className='profile'>
                 <h2>hello from profile</h2>
             </div>
         )
