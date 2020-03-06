4b120bcbbf280650a11324435c8d212623f1a8c5
Akhtam Ismatov <aismatov1991@gmail.com>
2020-03-05 20:15:30 -0800

    connect to  redux

--- frontend/components/profile/Profile.js
+++ frontend/components/profile/Profile.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
-import { fetchCourses } from '../../actions/courses'; 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCourses } from '../../actions/courses';
+
+const mdtp = dispatch => ({
+	fetchCourses: userId => dispatch(fetchCourses(userId))
+});
+
 class Profile extends Component {
-    render() {
-        return (
-            <div className='profile'>
-                <h2>hello from profile</h2>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className='profile'>
+				<h2>hello from profile</h2>
+			</div>
+		);
+	}
 }
 
 export default Profile;
