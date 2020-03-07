8c885e27eb76b3a6bc85b0d5279e73a61852212b
Akhtam Ismatov <aismatov1991@gmail.com>
2020-03-06 16:26:14 -0800

    add more user info on prifle pic

--- frontend/components/profile/Profile.js
+++ frontend/components/profile/Profile.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchCourses } from '../../actions/courses';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserNinja } from '@fortawesome/free-solid-svg-icons';
 
-const mdtp = dispatch => ({
-	fetchCourses: userId => dispatch(fetchCourses(userId))
-});
+import { fetchCourses } from '../../actions/courses';
 
 class Profile extends Component {
 	render() {
 		return (
 			<div className='profile'>
+				<div className='profile-header'>
+					<FontAwesomeIcon icon={faUserNinja} />
+					<h2></h2>
+				</div>
 				<h2>hello from profile</h2>
 			</div>
 		);
 	}
 }
+const mdtp = dispatch => ({
+	fetchCourses: userId => dispatch(fetchCourses(userId))
+});
+const mstp = state => {
+	const { firstName, lastName } = state.session;
+	return { firstName, lastName };
+};
 
 export default connect(null, mdtp)(Profile);
